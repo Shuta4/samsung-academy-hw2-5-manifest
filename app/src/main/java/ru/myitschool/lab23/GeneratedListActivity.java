9ae27b6d7b9508b715cf400203053de94e1af19b
myitschool <tegorkrsk@gmail.com>
2022-11-25 12:30:09 +0300

    Initial commit

---
+++ app/src/main/java/ru/myitschool/lab23/GeneratedListActivity.java
@@ -0,0 +1,15 @@
+package ru.myitschool.lab23;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GeneratedListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
